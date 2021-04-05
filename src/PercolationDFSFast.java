f9078ad1e02021902bd813f2baca2c1eaea71360
Zane Harrison <zane.harrison@duke.edu>
2021-04-05 16:36:36 -0400

    Hopefully finished PercolationUF (7.0)

--- src/PercolationDFSFast.java
+++ src/PercolationDFSFast.java
@@ -8,18 +8,19 @@ public class PercolationDFSFast extends PercolationDFS {
     protected void updateOnOpen(int row, int col) {
         if (row == 0) {
             dfs(row, col);
-        }
-        if (inBounds(row-1, col) && isFull(row-1, col)) {
-            dfs(row, col);
-        }
-        if (inBounds(row+1, col) && isFull(row+1, col)) {
-            dfs(row, col);
-        }
-        if (inBounds(row, col-1) && isFull(row, col-1)) {
-            dfs(row, col);
-        }
-        if (inBounds(row, col+1) && isFull(row, col+1)) {
-            dfs(row, col);
+        } else {
+            if (inBounds(row - 1, col) && isFull(row - 1, col)) {
+                dfs(row, col);
+            }
+            if (inBounds(row + 1, col) && isFull(row + 1, col)) {
+                dfs(row, col);
+            }
+            if (inBounds(row, col - 1) && isFull(row, col - 1)) {
+                dfs(row, col);
+            }
+            if (inBounds(row, col + 1) && isFull(row, col + 1)) {
+                dfs(row, col);
+            }
         }
     }
 }
